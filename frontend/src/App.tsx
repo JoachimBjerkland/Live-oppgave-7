30dfb15fc9f12b75d145142f236aa77678d79950
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2024-10-10 13:29:26 +0200

    update live

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,35 +2,72 @@ import { useState } from "react";
 import Grid from "./components/Grid";
 import Total from "./components/Total";
 import type { Student } from "./components/types";
+import AddStudentForm from "./components/AddStudentForm";
+import Filter from "./components/Filter";
+import Student from "./components/Student";
 
 const intitalStudent = [
-	{ id: "1", name: "Ola Normann" },
-	{ id: "2", name: "Kari Normann" },
+  { id: "1", name: "Ola Normann" },
+  { id: "2", name: "Kari Normann" },
 ];
 
 function App() {
-	// students ?? []
-	const [students, setStudents] = useState<Student[]>(intitalStudent ?? []);
+  const [filter, setFilter] = useState("-");
 
-	const onAddStudent = (student: Omit<Student, "id">) => {
-		setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
-	};
+  const [students, setStudents] = useState<Student[]>(intitalStudent ?? []);
 
-	const onRemoveStudent = (id: string) => {
-		setStudents((prev) => prev.filter((student) => student.id !== id));
-	};
+  // const options = Array.from(
+  //   new Set(
+  //     students.map((student) => student.name.trim().split(" ")[0].toLowerCase())
+  //   )
+  // );
 
-	return (
-		<main>
-			{/* <Student name="Marius" id="123" /> */}
-			<Grid
-				students={students}
-				onAddStudent={onAddStudent}
-				onRemoveStudent={onRemoveStudent}
-			/>
-			<Total total={students.length} />
-		</main>
-	);
+  const options = Array.from(
+    students
+      .reduce((acc, student: Student) => {
+        const name = student.name.trim().split(" ")[0];
+        if (acc.has(name)) return acc;
+
+        return acc.set(name, {
+          ...student,
+          value: name.toLowerCase(),
+          label: name,
+        });
+      }, new Map())
+      .values()
+  );
+
+  const onFilterChange = (filter: string) => {
+    setFilter(filter);
+  };
+
+  const onAddStudent = (student: Omit<Student, "id">) => {
+    setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
+  };
+
+  const onRemoveStudent = (id: string) => {
+    setStudents((prev) => prev.filter((student) => student.id !== id));
+  };
+
+  // [[key, {}], [key, {}]]
+  return (
+    <main>
+      {/* <Student name="Joachim" id="123" /> */}
+      <Filter
+        filter={filter}
+        onFilterChange={onFilterChange}
+        options={Object.values(options)}
+      />
+      <Grid
+        students={students}
+        // onAddStudent={onAddStudent}
+        onRemoveStudent={onRemoveStudent}
+      >
+        <AddStudentForm onAddStudent={onAddStudent} />
+      </Grid>
+      <Total total={students.length} />
+    </main>
+  );
 }
 
 export default App;
--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,35 +1,32 @@
 import Student from "./Student";
 import type { Student as StudentProp } from "./types";
-import AddStudentForm from "./AddStudentForm";
+// import AddStudentForm from "./AddStudentForm";
+import type { PropsWithChildren } from "react";
 
 type GridProps = {
-	students: StudentProp[];
-	onAddStudent: ({ name }: { name: string }) => void;
-	onRemoveStudent: (id: string) => void;
+  students: StudentProp[];
+  // onAddStudent: ({ name }: { name: string }) => void;
+  onRemoveStudent: (id: string) => void;
 };
 
-export default function Grid(props: GridProps) {
-	const { students, onAddStudent, onRemoveStudent } = props;
-	// props.students ?? []
-	// 	const [students, setStudents] = useState<StudentProp[]>(props.students ?? []);
+export default function Grid(props: PropsWithChildren<GridProps>) {
+  // const { students, onAddStudent, onRemoveStudent, children } = props;
+  const { students, onRemoveStudent, children } = props;
 
-	// const onAddStudent = (student: { name: string }) => {
-	// 	setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
-	// };
-
-	return (
-		<section>
-			<article className="grid">
-				{students.map((student) => (
-					<Student
-						key={student.id}
-						name={student.name}
-						id={student.id}
-						onRemoveStudent={onRemoveStudent}
-					/>
-				))}
-			</article>
-			<AddStudentForm onAddStudent={onAddStudent} />
-		</section>
-	);
+  return (
+    <section>
+      <article className="grid">
+        {students.map((student) => (
+          <Student
+            key={student.id}
+            name={student.name}
+            id={student.id}
+            onRemoveStudent={onRemoveStudent}
+          />
+        ))}
+      </article>
+      {/* <AddStudentForm onAddStudent={onAddStudent} /> */}
+      {children}
+    </section>
+  );
 }
